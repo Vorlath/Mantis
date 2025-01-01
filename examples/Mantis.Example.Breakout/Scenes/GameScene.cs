494fb115ec4c1663170d2ea1d9d22edfbc57b0ef
JohnSmidt <johnmsmidt@gmail.com>
2025-01-01 13:26:58 -0700

    #15 - Added collision to blocks

--- examples/Mantis.Example.Breakout/Scenes/GameScene.cs
+++ examples/Mantis.Example.Breakout/Scenes/GameScene.cs
@@ -34,45 +34,12 @@ namespace Mantis.Example.Breakout.Scenes
             entityInitializer.Init(new Velocity(100, 200));
             entityInitializer.Init(new Size(32, 32));
 
-            entityInitializer = entityFactory.BuildEntity<BallDescriptor>(1, ExclusiveGroups.ballGroup);
-            entityInitializer.Init(new Position(200, 200));
-            entityInitializer.Init(new Velocity(100, 200));
-            entityInitializer.Init(new Size(32, 32));
-
-            entityInitializer = entityFactory.BuildEntity<BallDescriptor>(2, ExclusiveGroups.ballGroup);
-            entityInitializer.Init(new Position(300, 200));
-            entityInitializer.Init(new Velocity(100, 200));
-            entityInitializer.Init(new Size(32, 32));
-
             //block
             entityInitializer = entityFactory.BuildEntity<BlockDescriptor>(3, ExclusiveGroups.blockGroup);
             entityInitializer.Init(new Position(300, 100));
             entityInitializer.Init(new Size(64, 32));
             entityInitializer.Init(new Health(1));
             entityInitializer.Init(new Collidable());
-
-            entityInitializer = entityFactory.BuildEntity<BlockDescriptor>(4, ExclusiveGroups.blockGroup);
-            entityInitializer.Init(new Position(500, 100));
-            entityInitializer.Init(new Size(64, 32));
-            entityInitializer.Init(new Health(1));
-            entityInitializer.Init(new Collidable());
-
-            entityInitializer = entityFactory.BuildEntity<BlockDescriptor>(5, ExclusiveGroups.blockGroup);
-            entityInitializer.Init(new Position(300, 300));
-            entityInitializer.Init(new Size(64, 32));
-            entityInitializer.Init(new Health(1));
-            entityInitializer.Init(new Collidable());
-
-            entityInitializer = entityFactory.BuildEntity<BlockDescriptor>(6, ExclusiveGroups.blockGroup);
-            entityInitializer.Init(new Position(500, 300));
-            entityInitializer.Init(new Size(64, 32));
-            entityInitializer.Init(new Health(1));
-            entityInitializer.Init(new Collidable());
-
-            entityInitializer = entityFactory.BuildEntity<BallDescriptor>(7, ExclusiveGroups.ballGroup);
-            entityInitializer.Init(new Position(400, 200));
-            entityInitializer.Init(new Velocity(100, 200));
-            entityInitializer.Init(new Size(32, 32));
         }
 
         public void Draw(GameTime gameTime)
