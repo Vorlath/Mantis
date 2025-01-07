9e9f0c2db41ff656abb27131646d8bb3354b6ea7
rettoph <anthony@turner.ec>
2025-01-07 13:49:01 -0700

    Add new class for linting

---
+++ examples/Mantis.Example.Breakout/Engines/BadEngine.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mantis.Example.Breakout.Engines
+{
+    public class badEngine
+    {
+        private int Age;
+
+        public badEngine()
+        {
+            Age = 5;
+        }
+    }
+}
