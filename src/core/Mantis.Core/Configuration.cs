8dc25512ce7a9b48f16d7782ecc6ea49d5979dc8
rettoph <anthony@turner.ec>
2025-01-07 22:13:52 -0700

    #32 - Add basic configuration api

---
+++ src/core/Mantis.Core/Configuration.cs
@@ -0,0 +1,11 @@
+﻿using Mantis.Core.Common;
+using Mantis.Core.Common.Services;
+
+namespace Mantis.Core
+{
+    public class Configuration<T>(IConfigurationService configurationService) : IConfiguration<T>
+        where T : class, new()
+    {
+        public T Value { get; set; } = configurationService.Configure<T>();
+    }
+}
---
+++ src/core/Mantis.Core/Services/ConfigurationService.cs
@@ -0,0 +1,30 @@
+﻿using System.Runtime.InteropServices;
+using Autofac;
+using Mantis.Core.Common;
+using Mantis.Core.Common.Services;
+
+namespace Mantis.Core.Services
+{
+    public class ConfigurationService(ILifetimeScope scope, IEnumerable<Configurator> configurators) : IConfigurationService
+    {
+        private readonly ILifetimeScope _scope;
+        private readonly Configurator[] _configurators = [.. configurators];
+        private readonly Dictionary<Type, Configurator[]> _configuratorsByType = [];
+
+        public void Configure<T>(T instance)
+            where T : class
+        {
+            ref Configurator[]? configurators = ref CollectionsMarshal.GetValueRefOrAddDefault(this._configuratorsByType, typeof(T), out bool exists);
+            if (exists == false)
+            {
+                configurators = [.. this._configurators.Where(c => c.TryConfigureInstance(this._scope, instance))];
+                return;
+            }
+
+            foreach (Configurator configurator in configurators!)
+            {
+                configurator.TryConfigureInstance(this._scope, instance);
+            }
+        }
+    }
+}
