2ef49fa2e1d92aab9b0f76c4e76f96c7986896b2
rettoph <anthony@turner.ec>
2025-01-13 20:39:28 -0700

    #32 - Code cleanup

--- src/core/Mantis.Core/Extensions/ContainerBuilderExtensions.cs
+++ src/core/Mantis.Core/Extensions/ContainerBuilderExtensions.cs
@@ -7,7 +7,7 @@ namespace Mantis.Core.Extensions
 {
     public static class ContainerBuilderExtensions
     {
-        public static ContainerBuilder RegisterCoreServices<T>(this ContainerBuilder builder, Action<ILifetimeScope, T> configurator)
+        public static ContainerBuilder RegisterCoreServices<T>(this ContainerBuilder builder)
             where T : class, new()
         {
             builder.RegisterType<ConfigurationService>().As<IConfigurationService>().InstancePerLifetimeScope();
