a1305325cfc89111fc5b40fad2bbd5749d38d326
tony <anthony@turner.ec>
2025-05-04 23:55:39 -0600

    #61 - Ensure analyzer packages are built in github workflows

--- scripts/install-workflow.sh
+++ scripts/install-workflow.sh
@@ -11,8 +11,24 @@
 MONOGAME_BUILD_VERSION="1.3.3.7-cpt"
 
 WORKING_DIRECTORY=$(pwd)
-SCRIPT_DIR=$(dirname "$(realpath "$0")")
-MONOGAME_DIRECTORY=$(realpath "$SCRIPT_DIR/../libraries/MonoGame")
+SOLUTION_DIRECTORY=$(dirname $(dirname "$(realpath "$0")"))
+ANALYZER_DIRECTORY="$(realpath "$SOLUTION_DIRECTORY/analyzers")"
+NUGET_DIRECTORY="$SOLUTION_DIRECTORY/.nuget"
+MONOGAME_DIRECTORY=$(realpath "$SOLUTION_DIRECTORY/libraries/MonoGame")
+
+# ----------------------
+# BEGIN PACKAGE BUILDING
+# ----------------------
+
+# Confirm .nuget directory exists
+if [ ! -d "$NUGET_DIRECTORY" ]; then
+    mkdir -p "$NUGET_DIRECTORY"
+fi
+
+# Build Analyzer package
+dotnet build -c Release "$ANALYZER_DIRECTORY/Mantis.Analyzer.Core.Common"
+dotnet pack -o "$NUGET_DIRECTORY" /p:Version="$ANALYZER_VERSION" "$ANALYZER_DIRECTORY/Mantis.Analyzer.Core.Common"
+echo $ANALYZER_DIRECTORY
 
 # ---------------------------
 # BEGIN MONOGAME INSTALLATION
@@ -33,3 +49,32 @@ cd "$WORKING_DIRECTORY"
 
 # Install new tools
 dotnet tool install --local --create-manifest-if-needed --add-source "$MONOGAME_DIRECTORY/artifacts/NuGet" dotnet-mgcb
+
+# ---------------------
+# BEGIN FILE GENERATION
+# ---------------------
+
+cat <<EOF > "$SOLUTION_DIRECTORY/Mantis.Core.Common.g.targets"
+<!-- Generated via Mantis/scripts/install.sh -->
+<Project>
+
+  <PropertyGroup>
+    <TreatWarningsAsErrors>true</TreatWarningsAsErrors>
+  </PropertyGroup>
+
+  <ItemGroup>
+    <!-- Mantis packages are added to a local nuget feed via the 'Mantis/nuget.config' file -->
+    <PackageReference Include="Mantis.Analyzer.Core.Common" Version="$ANALYZER_VERSION" />
+  </ItemGroup>
+
+</Project>
+EOF
+
+cat <<EOF > "$SOLUTION_DIRECTORY/nuget.config"
+<!-- Generated via Mantis/scripts/install.sh -->
+<configuration>
+  <packageSources>
+    <add key="MantisPackages" value="$NUGET_DIRECTORY" />
+  </packageSources>
+</configuration>
+EOF
