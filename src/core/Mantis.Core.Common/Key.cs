8ef735689ecda8cd9ad551073e4f271349e1cca4
Tony <anthony@turner.ec>
2025-05-21 20:43:16 -0600

    #71 - Create basic deterministic Key and non deterministic Id

---
+++ src/core/Mantis.Core.Common/Key.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Standart.Hash.xxHash;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace Mantis.Core.Common
+{
+    /// <summary>
+    /// A deterministic unique identifier that maps to a
+    /// Type + string 
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public struct Key<T> : IEquatable<Key<T>>
+    {
+        private readonly uint128 _value;
+
+        private static readonly Dictionary<uint128, string> _names = [];
+        private static readonly Dictionary<string, Key<T>> _ids = [];
+
+        public readonly string Name => _names[this._value];
+
+        private Key(string name)
+        {
+            this._value = xxHash128.ComputeHash(name);
+            Key<T>._names.Add(this._value, name);
+        }
+
+        public static Key<T> GetByName(string name)
+        {
+            if(_ids.TryGetValue(name, out Key<T> id) == false)
+            {
+                id = new Key<T>(name);
+                Key<T>._ids.Add(name, id);
+            }
+
+            return id;
+        }
+
+        #region Overrides and Equality
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(this._value);
+        }
+
+        public bool Equals(Key<T> other)
+        {
+            return this._value.high64 == other._value.high64
+                && this._value.low64 == other._value.low64;
+        }
+
+        public override bool Equals(object? obj)
+        {
+            return obj is Key<T> && this.Equals((Key<T>)obj);
+        }
+        public static bool operator ==(Key<T> left, Key<T> right)
+        {
+            return left.Equals(right);
+        }
+
+        public static bool operator !=(Key<T> left, Key<T> right)
+        {
+            return !(left == right);
+        }
+        #endregion
+    }
+}
